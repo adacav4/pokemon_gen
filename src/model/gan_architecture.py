2b6a430bf2c26b2d2d6bad61268b3ce5a4e2c31b
Adarsh Cavale <aac90@pitt.edu>
2023-11-08 16:46:55 -0500

    change to layer norm in generator

--- src/model/gan_architecture.py
+++ src/model/gan_architecture.py
@@ -13,21 +13,21 @@ class Generator(nn.Module):
         self.fc_out = nn.Linear(512, output_dim)
 
         self.dropout = nn.Dropout(dropout_rate)
-        self.batch_norm1 = nn.BatchNorm1d(128)
-        self.batch_norm2 = nn.BatchNorm1d(256)
-        self.batch_norm3 = nn.BatchNorm1d(512)
+        self.layer_norm1 = nn.LayerNorm(128)
+        self.layer_norm2 = nn.LayerNorm(256)
+        self.layer_norm3 = nn.LayerNorm(512)
 
     def forward(self, x):
         x1 = F.leaky_relu(self.fc1(x), 0.1)
-        x1 = self.batch_norm1(x1)
+        x1 = self.layer_norm1(x1)
         x1 = self.dropout(x1)
 
         x2 = F.leaky_relu(self.fc2(torch.cat([x, x1], dim=1)), 0.1)
-        x2 = self.batch_norm2(x2)
+        x2 = self.layer_norm2(x2)
         x2 = self.dropout(x2)
 
         x3 = F.leaky_relu(self.fc3(torch.cat([x1, x2], dim=1)), 0.1)
-        x3 = self.batch_norm3(x3)
+        x3 = self.layer_norm3(x3)
         x3 = self.dropout(x3)
 
         return torch.tanh(self.fc_out(x3))
